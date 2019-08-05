7f526b0332f54091e5b6bb280937107b575fda6a
aminamos <26092352+aminamos@users.noreply.github.com>
2019-08-05 14:04:07 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main = document.querySelector("#main");
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerText = " is the champion";
+
+newHeader = document.body.appendChild(newHeader);
